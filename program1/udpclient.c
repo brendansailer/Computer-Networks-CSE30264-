e3bb2fd7f375fd08a07a072f43bb7e7fec491ebd
Brendan Sailer <bsailer1@nd.edu>
2020-08-27 16:05:01 -0400

    Progress

--- program1/udpclient.c
+++ program1/udpclient.c
@@ -1,3 +1,10 @@
+/*
+	Brendan Sailer
+	Program 1 - Computer Networks
+
+	Run: ./udpclient <Host Name> <Port Number> <Text to Send>
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +15,6 @@
 #include <pg1lib.h>
 
 #define SERVER_PORT 41042
-#define MAX_LINE 256
 
 void client(char * argv[]);
 
@@ -26,13 +32,12 @@ void client(char * argv[]){
 	struct hostent *hp;
 	struct sockaddr_in sin;
 	char *host;
-	char buf[MAX_LINE];
+	char buf[BUFSIZ];
 	int s, len;
-
-	//host = argv[1];
-	host = "student04.cse.nd.edu";
+	unsigned long check_sum;
 
 	/* translate host name into peer's IP address */
+	host = argv[1];
 	hp = gethostbyname(host);
 	if (!hp) {
 		fprintf(stderr, "udpclient: unknown host: %s\n", host);
@@ -46,20 +51,63 @@ void client(char * argv[]){
 	sin.sin_port = htons(SERVER_PORT);
 
 	/* active open */
-	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+	if((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
 		fprintf(stderr, "udpclient: failed to open socket\n");
 		exit(1);
 	}
-	printf("Welcome to udplient, type \'Exit\' to quit.\n");
 
-	/* main loop: get and send lines of text */
-	while (fgets(buf, sizeof(buf), stdin)) {
-		buf[MAX_LINE-1] = '\0';
-		if (!strncmp(buf,"Exit",4)) break;
-			len = strlen(buf) + 1;
-		if(sendto(s, buf, len, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr))==-1){
-			perror ("Client Send Error!\n");
+	/* Generate and send public key */
+	char *pubKey = getPubKey();
+	len = strlen(pubKey) + 1;
+	if(sendto(s, pubKey, len, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr))==-1){
+		fprintf(stderr, "udpclient: failed to send public key\n");
+		exit(1);
+	}
+	
+	/* Receive and decrypt public key */
+	if(recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&sin, sizeof(sin))==-1){
+		fprintf(stderr, "udpclient: failed to receive encrypted key\n");
+		exit(1);
+	}
+
+	char* input = argv[3];
+	FILE* inputFile = fopen(input, "r");
+
+	if(inputFile){
+		printf("Sending file\n");
+		if(fread(buf, sizeof(char), BUFSIZ, inputFile) <= 0){
+			fprintf(stderr, "udpclient: failed to read from file\n");
 			exit(1);
 		}
+		fclose(inputFile);
+	} else{
+		printf("Sending string\n");
+		memcpy(buf, input, sizeof(input));
 	}
+	buf[BUFSIZ-1] = '\0';
+
+	check_sum = checksum(buf);
+	printf("Checksum sent: %lu\n", check_sum);	
+	
+	len = strlen(buf) + 1;
+	if(sendto(s, buf, len, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr))==-1){
+		fprintf(stderr, "udpclient: failed to send data\n");
+		exit(1);
+	}
+
+	shutdown(s, 0);
+	/*
+	// main loop: get and send lines of text
+	while (fgets(buf, sizeof(buf), stdin)) {
+		buf[BUFSIZ-1] = '\0';
+		if (!strncmp(buf,"Exit",4)){
+			break;
+		}
+		len = strlen(buf) + 1;
+		if(sendto(s, buf, len, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr))==-1){
+			fprintf(stderr, "udpclient: failed to send data\n");
+			exit(1);
+		}
+		printf("length: %d\n", len);
+	} */
 }
--- program1/udpserver.c
+++ program1/udpserver.c
@@ -1,3 +1,10 @@
+/*
+	Brendan Sailer
+	Program 1 - Computer Networks
+
+	Run: ./updserver <Port Number>
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,54 +14,83 @@
 #include <netdb.h>
 #include <pg1lib.h>
 
-#define SERVER_PORT 41042
-#define MAX_LINE 256
-#define MAX_PENDING 5
-
 void server(char * argv[]);
 
 int main(int argc, char * argv[]) {
 	if (argc == 2) {
 		server(argv);
 	} else {
-		fprintf(stderr, "udpclient <Host Name> <Port Number> <Text to Send>\n");
+		fprintf(stderr, "udpserver <Port Number> \n");
 		exit(1);
 	}
 }
 
 void server(char * argv[]){
 	struct sockaddr_in sin, client_addr;
-	char buf[MAX_LINE];
-	int len,addr_len;
+	struct timeval start_time;
+	struct tm *date;
+	time_t t;
+	unsigned long check_sum;
+	char buf[BUFSIZ];
+	int len, addr_len;
 	int s;
+
+	int port = atoi(argv[1]); 
 
 	//* build address data structure */
 	bzero((char *)&sin, sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = INADDR_ANY; //Use the default IP address of server
-	sin.sin_port = htons(SERVER_PORT);
-	printf("Waiting ...\n");
+	sin.sin_port = htons(port);
+	printf("Waiting on port: %d ...\n", port);
+
+	/* Generate public key */
+	char *pubKey = getPubKey();
 
 	/* setup passive open */
-	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		fprintf(stderr, "udpclient: failed to open socket\n");
+	if((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		fprintf(stderr, "udpserver: failed to open socket\n");
 		exit(1);
 	}
-
-	if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
-		fprintf(stderr, "udpclient: failed to bind\n");
+	
+	if((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+		fprintf(stderr, "udpserver: failed to bind\n");
 		exit(1);
 	}
-
-	addr_len = sizeof (client_addr);
+	
+	addr_len = sizeof(client_addr);
 
 	while (1){
-		printf("TEST123");
 		if(recvfrom(s, buf, sizeof(buf), 0,  (struct sockaddr *)&client_addr, &addr_len)==-1){
-			fprintf(stderr, "udpclient: failed to receive message\n");
+			fprintf(stderr, "udpserver: failed to receive public key\n");
     	exit(1);
 		}
-		printf("Server Recived:%s\n", buf);
+		printf("Public Key: %s\n", buf);
+		bzero((char*)&buf, sizeof(buf));
+
+		char* encryptedKey = encrypt(buf, pubKey);
+		len = strlen(encryptedKey)
+		if(sendto(s, encryptedKey, len, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr))==-1){
+			fprintf(stderr, "udpserver: failed to send encrypted key\n");
+    	exit(1);
+		}
+		
+		if(recvfrom(s, buf, sizeof(buf), 0,  (struct sockaddr *)&client_addr, &addr_len)==-1){
+			fprintf(stderr, "udpserver: failed to receive message\n");
+    	exit(1);
+		}
+
+		gettimeofday(&start_time, NULL);
+		t = start_time.tv_sec;
+		date = localtime(&t);
+
+		check_sum = checksum(buf);
+
+		printf("*** New Message ***\n");
+		printf("Received Time: %s", asctime(date));
+		printf("Received Message:\n%s\n", buf);
+		printf("Calculated Checksum: %lu\n", check_sum);
+		printf("\n");
 		bzero((char*)&buf, sizeof(buf));
 	}
 	close(s);
